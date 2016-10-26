cb26949307b8aa25190ee9ef34ff41881c4165f4
Ender <enderyurt@gmail.com>
2016-10-26 15:16:04 +0300

    Change color.

--- lib/hurriyet_cli/columns.rb
+++ lib/hurriyet_cli/columns.rb
@@ -19,7 +19,7 @@ module HurriyetCli
     def formated_parse(columns)
       columns.each.with_index(1) do |a, i|
         created_at = Time.parse(a["CreatedDate"]).strftime("%d/%m/%y | %H:%M")
-        @display.puts "#{i} - #{Rainbow(a["Title"]).red} - #{Rainbow(a["Fullname"]).orange} - #{Rainbow(created_at).color(:dimgray)}"
+        @display.puts "#{i} - #{Rainbow(a["Title"]).red} - #{Rainbow(a["Fullname"]).orange} - #{Rainbow(created_at).color(:blue)}"
         @display.puts Rainbow(a["Description"]).color(:dimgray)
         @display.puts Rainbow(a["Url"]).cyan
         @display.puts ""
