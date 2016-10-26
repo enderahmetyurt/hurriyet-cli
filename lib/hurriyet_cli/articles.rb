298ee5fbf50a20804edfde2a6716ca02853d467e
Ender <enderyurt@gmail.com>
2016-10-26 15:14:13 +0300

    Change color.

--- lib/hurriyet_cli/articles.rb
+++ lib/hurriyet_cli/articles.rb
@@ -19,7 +19,7 @@ module HurriyetCli
     def formated_parse(articles)
       articles.each.with_index(1) do |a, i|
         created_at = Time.parse(a["CreatedDate"]).strftime("%d/%m/%y | %H:%M")
-        @display.puts "#{i} - #{Rainbow(a["Title"]).red} - #{Rainbow(created_at).color(:dimgray)}"
+        @display.puts "#{i} - #{Rainbow(a["Title"]).red} - #{Rainbow(created_at).color(:yellow)}"
         @display.puts Rainbow(a["Description"]).color(:dimgray)
         @display.puts Rainbow(a["Url"]).cyan
         @display.puts ""
